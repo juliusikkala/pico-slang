1ebe975dad9337ed8c4dac54cce84fa7e903e7ff
Julius Ikkala <julius.ikkala@gmail.com>
2026-01-02 16:43:39 +0200

    Make build setup allow multiple modules

--- cmake/CMakeSlangInformation.cmake
+++ cmake/CMakeSlangInformation.cmake
@@ -11,10 +11,14 @@ set(CMAKE_Slang_DEPENDS_USE_COMPILER TRUE)
 set(CMAKE_DEPFILE_FLAGS_Slang "-depfile <DEP_FILE>")
 
 set(CMAKE_Slang_COMPILE_OBJECT
-    "<CMAKE_Slang_COMPILER> -target host-object-code -emit-cpu-via-llvm <DEFINES> <INCLUDES> <FLAGS> <SOURCE> -o <OBJECT>"
+    "<CMAKE_Slang_COMPILER> <DEFINES> <INCLUDES> <FLAGS> <SOURCE> -o <OBJECT>"
 )
 
-set(CMAKE_Slang_OUTPUT_EXTENSION ".o")
+set(CMAKE_Slang_OUTPUT_EXTENSION "-module")
 
-set(CMAKE_Slang_LINKER_PREFERENCE C) 
+set(CMAKE_Slang_CREATE_STATIC_LIBRARY
+    "<CMAKE_Slang_COMPILER> -target host-object-code -emit-cpu-via-llvm -llvm-target-triple thumb-v8-eabi -llvm-cpu cortex-m33 -llvm-features armv8-m.main,fp-armv8,dsp -Xllvm... -function-sections -data-sections -float-abi=soft --stack-size-section -X. <CMAKE_Slang_LINK_FLAGS> <LINK_FLAGS> <OBJECTS> -o <TARGET>"
+)
+
+set(CMAKE_Slang_LINKER_PREFERENCE Slang) 
 set(CMAKE_Slang_INFORMATION_LOADED 1)
