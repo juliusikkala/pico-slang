f8a451ab7468eee5b5818f1a0145ae17f0aa3b2b
Julius Ikkala <julius.ikkala@gmail.com>
2026-01-02 21:28:58 +0200

    Render Mandelbrot on display

---
+++ export_inline_functions.c
@@ -0,0 +1,6 @@
+// I'm so, so sorry... But I really need the inline functions in an object file
+// for the Slang bindings to work.
+#define static extern
+#include <hardware/spi.h>
+#include <hardware/gpio.h>
+#include <pico/time.h>
